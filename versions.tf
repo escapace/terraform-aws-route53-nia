09df59e9299cc2e52bebac0922ed8107ac35d149
Mark Martirosian <hello@markmartirosian.com>
2023-11-09 13:09:25 +0000

    chore: typo

--- versions.tf
+++ versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.24.0"
+      version = "~>5.24.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~>3.5.1"
     }
   }
 }
